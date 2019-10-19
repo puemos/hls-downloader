34d20415687c0697837c7d56fa2b110c7f767b7e
aaimtt <aaimtt@users.noreply.github.com>
2019-10-19 18:22:14 +0200

    download video with default filename
    Signed-off-by: aaimtt <aaimtt@users.noreply.github.com>

--- src/background/saveAs.js
+++ src/background/saveAs.js
@@ -1,22 +1,42 @@
-import { takeEvery } from "redux-saga/effects";
+import { takeEvery, call } from "redux-saga/effects";
 
-function getFilename(download) {
-  return [download.title]
-    .filter(Boolean)
-    .map(str => String(str))
-    .map(str => str.split("/"))
-    .filter(str => str[str.length - 1])
-    .map(str => str[str.length - 1])
-    .map(str => str.split(".m3u8"))
-    .map(str => str[0])
-    .map(str => `${str}.mp4`)[0];
+function clearFileName(name) {
+    return name.replace(/\s+/gi, '-').replace(/[^a-zA-Z0-9\-]/gi, '');
 }
 
-function saveAs(action) {
+function getActiveTabTitle() {
+    return new Promise((res, rej)=>{
+        try {
+            chrome.tabs.getSelected(({ title: tabTitle = null}) => {
+                res(tabTitle);
+            } )
+        } catch (error) {
+            rej(error)
+        }
+
+    })
+}
+
+function* getFilename(download) {
+    const tabTitle = yield call(getActiveTabTitle)
+
+    return clearFileName(tabTitle || 
+        [download.title]
+            .filter(Boolean)
+            .map(str => String(str))
+            .map(str => str.split("/"))
+            .filter(str => str[str.length - 1])
+            .map(str => str[str.length - 1])
+            .map(str => str.split(".m3u8"))
+            .map(str => str[0])
+            .map(str => `${str}.mp4`)[0]);
+}
+
+function* saveAs(action) {
   const { payload: download } = action;
   chrome.downloads.download({
     url: download.link,
-    filename: getFilename(download),
+    filename: yield getFilename(download),
     saveAs: true
   });
 }
