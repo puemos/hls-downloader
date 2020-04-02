9d9525da77cf78578f454cfafd4457bd3912eeb8
Shy <puemos@gmail.com>
2020-04-02 19:19:15 +0200

    change: parcel>webpack, remove: debugger, webRequestBlocking permissions

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Store } from "react-chrome-redux";
+import { Store } from "webext-redux";
 import { render } from "react-dom";
 import { Provider } from "react-redux";
 import { ThemeProvider } from "styled-components";
