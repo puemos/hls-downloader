84bc1b64f0570ff5eb95404ce58ed0de612f2096
Shy <puemos@gmail.com>
2020-04-05 14:55:55 +0200

    fix: add .gitignore

--- src/core/node_modules/hls.js/scripts/get-version-tag.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-const versionParser = require('./version-parser.js');
-const packageJson = require('../package.json');
-
-console.log(versionParser.getVersionTag('v' + packageJson.version));
--- src/core/node_modules/hls.js/src/utils/mediasource-helper.ts
+++
@@ -1,7 +0,0 @@
-/**
- * MediaSource helper
- */
-
-export function getMediaSource (): typeof MediaSource | undefined {
-  return (window as any).MediaSource || (window as any).WebKitMediaSource;
-}
