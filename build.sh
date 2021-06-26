32d79e24696b8e710d6e2bcd47a346efa7a5659a
Shy <puemos@gmail.com>
2021-06-26 11:47:34 +0200

    fix: zip packing

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@ rm -rf extension-archive.zip
 
 (cd ./src/extension/ && sh ./update-core.sh)
 (cd ./src/extension/ && sh ./build-extension.sh)
-(cd ./src/extension/ && zip -r extension-archive.zip ./dist/**)
+(cd ./src/extension/dist/ && zip -r extension-archive.zip .)
 mv ./src/extension/extension-archive.zip extension-archive.zip
