dad0da9b4ada5a6ed85b70079aaec3fcc1b8eaa9
Shy <puemos@gmail.com>
2021-06-26 11:51:18 +0200

    fix: type in build.sh

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ rm -rf extension-archive.zip
 (cd ./src/extension/ && sh ./update-core.sh)
 (cd ./src/extension/ && sh ./build-extension.sh)
 (cd ./src/extension/dist/ && zip -r extension-archive.zip .)
-mv ./src/extension/extension-archive.zip extension-archive.zip
+mv ./src/extension/dist/extension-archive.zip extension-archive.zip
