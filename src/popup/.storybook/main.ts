3679d5326c962a50cc10a2c2e2df09a8853201f2
Shy Alter <puemos@users.noreply.github.com>
2025-07-28 22:48:25 +0200

    fix storybook start

--- src/popup/.storybook/main.ts
+++ src/popup/.storybook/main.ts
@@ -1,4 +1,6 @@
 import type { StorybookConfig } from '@storybook/react-vite';
+import { mergeConfig } from 'vite';
+import { resolve } from 'path';
 
 const config: StorybookConfig = {
   stories: [
@@ -9,6 +11,15 @@ const config: StorybookConfig = {
   framework: {
     name: '@storybook/react-vite',
     options: {}
+  },
+  viteFinal: async (config) => {
+    return mergeConfig(config, {
+      resolve: {
+        alias: {
+          '@hls-downloader/design-system': resolve(__dirname, '../../design-system/src')
+        }
+      }
+    });
   },
   docs: {
     autodocs: 'tag'
